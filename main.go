80f92a5b936cc23e2a01d58d3560743976a6f6dc
megamxl <maximilian.nowak@outlook.at>
2025-11-29 17:22:43 +0100

    Docker it

--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"embed"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
 //go:embed static templates
 var content embed.FS
+var contentFS, _ = fs.Sub(content, "static")
 
 func main() {
 
 	InitComments()
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(contentFS))))
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/more-bears", handleMoreBears)
 	http.HandleFunc("/show-comments", handleCommentsSeen)
